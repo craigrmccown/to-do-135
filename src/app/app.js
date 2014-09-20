1ecf0e6f69c876ee15add4ebee0c7bd90c713b0e
Craig McCown <craigrmccown@gmail.com>
2014-09-20 16:51:44 -0400

    filled in boilerplate

---
+++ src/app/app.js
@@ -0,0 +1,5 @@
+angular.module('toDo135.app', ['ui.router'])
+  .config([function() {
+  }])
+  .run([function() {
+  }]);
---
+++ src/app/mock.js
@@ -0,0 +1,5 @@
+angular.module('toDo135.mock', ['toDo135.app', 'ngMockE2E'])
+  .config([function() {
+  }])
+  .run([function() {
+  }]);
