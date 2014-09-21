a8f117c876444ff2a55da28974d359840c070e6d
Craig McCown <craigrmccown@gmail.com>
2014-09-21 09:55:25 -0400

    views working

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,25 @@
 angular.module('toDo135.app', ['ui.router'])
-  .config([function() {
+  .config(['$stateProvider', function($stateProvider) {
+    $stateProvider
+      .state('app', {
+        templateUrl: '/app/views/app.html',
+        controller: 'AppCtrl',
+        abstract: true
+      })
+      .state('app.todo', {
+        views: {
+          'bank': {
+            templateUrl: '/app/todo/views/bank.html',
+          },
+          'day': {
+            templateUrl: '/app/todo/views/day.html'
+          }
+        }
+      })
   }])
-  .run([function() {
+  .run(['$state', function($state) {
+    $state.go('app.todo');
+  }])
+  .controller('AppCtrl', [function() {
+
   }]);
--- src/app/mock.js
+++ src/app/mock.js
@@ -1,5 +1,6 @@
 angular.module('toDo135.mock', ['toDo135.app', 'ngMockE2E'])
   .config([function() {
   }])
-  .run([function() {
+  .run(['$httpBackend', function($httpBackend) {
+    $httpBackend.whenGET(/.+\.html/).passThrough();
   }]);
